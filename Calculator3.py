ccc0d107f30893c6a39033a751b9df8752eba591
Raza <razarajpoot99598@gmail.com>
2023-04-09 13:33:47 +0500

    Project 3 added 'Calculator'

---
+++ Calculator3.py
@@ -0,0 +1,58 @@
+def add(num1, num2):
+    
+    sum = num1+num2
+    print(sum)
+
+def mul(num1, num2):
+    mul = num1*num2
+    print(mul)
+    
+def sub(num1, num2):
+    sub = num1-num2
+    print(sub)
+    
+def div(num1, num2):
+    
+    div = num1/num2
+    print(div) 
+    
+    
+    
+while(True):
+    print("Press 1 for addition")
+    print("Press 2 for substraction")
+    print("Press 3 for multiplication")
+    print("Press 4 for division")
+    print("type exit to close the application")
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        num1 = int(input("Enter the first number: "))       
+        num2 = int(input("Enter the second number: "))
+        
+        add(num1, num2)
+    elif choice == '2':
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+        
+        sub(num1, num2)    
+    
+    elif choice == '3':
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+        
+        mul(num1,num2)
+        
+    elif choice == '4':
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+        
+        div(num1, num2)        
+        
+    elif choice == "exit":
+        print("Thank you for using our application")
+        break    
+    else:
+        print("You entered the Invalid choice, Please try again with valid choice.")
+        
+        
